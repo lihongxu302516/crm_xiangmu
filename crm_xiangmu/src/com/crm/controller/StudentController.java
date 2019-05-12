4d910a35fa3ceb3defb0f287a9f8ac5f4e4626d7
zhaowenlulu <1192885519qq.com>
2019-05-12 13:01:17 +0800

    crm_xiangmu
    2019-5-12 13:01

--- crm_xiangmu/src/com/crm/controller/StudentController.java
+++ crm_xiangmu/src/com/crm/controller/StudentController.java
@@ -1,8 +1,33 @@
 package com.crm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.crm.entity.Fenye;
+import com.crm.entity.Student;
+import com.crm.service.StudentService;
 
 @Controller
 public class StudentController {
+	@Autowired
+	private StudentService studentService;
+	@Autowired
+	private Fenye<Student> fenye;
 	
+	@RequestMapping(value = "student_guanli")
+	public String student() {
+		return "student_guanli";
+	}
+	@RequestMapping(value = "student_xianshi",method = RequestMethod.POST)
+	@ResponseBody
+	public Fenye<Student> student_xianshi(Integer page,Integer rows,Student student){
+		fenye.setPage(page);
+		fenye.setLimit(rows);
+		fenye.setT(student);
+		fenye = studentService.selesctStudent(fenye);
+		return fenye;
+	}
 }
