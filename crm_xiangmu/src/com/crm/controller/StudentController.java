54f4c24593fe4b90b2d823afe1ddd7655f843781
zhaowenlulu <1192885519qq.com>
2019-05-13 11:36:37 +0800

    crm_xiangmu
    2019-5-13 11:36

--- crm_xiangmu/src/com/crm/controller/StudentController.java
+++ crm_xiangmu/src/com/crm/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.crm.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,7 @@ public class StudentController {
 	private Fenye<Student> fenye;
 	
 	@RequestMapping(value = "student_guanli")
-	public String student() {
+	public String student(HttpServletRequest request) {
 		return "student_guanli";
 	}
 	@RequestMapping(value = "student_xianshi",method = RequestMethod.POST)
